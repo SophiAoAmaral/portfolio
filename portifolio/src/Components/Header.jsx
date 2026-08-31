a5a6889c4875ab31662868b01af00b651193aed5
sophia <sophiaoliveira2706@outlook.com>
2026-08-31 12:55:17 -0300

    Configurando GitHub Pages

--- portifolio/src/Components/Header.jsx
+++ portifolio/src/Components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className='border-b border-rosa/30 bg-fundo'>
         <nav className='container flex justify-between items-center py-7'>
-            <Link to='/' className='detail relative text-xl font-semibold text-texto'>Sophia Oliveira</Link>
+            <Link to='/' className='detail1 relative text-xl font-semibold text-texto'>Sophia Oliveira</Link>
             <ul className='flex items-center gap-5 text-texto/70 *:cursor-pointer *:transition [&>li]:hover:text-rosa'>
                 <a href='#contato' className='border border-rosa px-4 py-1 rounded-2xl text-rosa hover:bg-rosa/30 hover:text-texto '>Contato</a>
             </ul>
